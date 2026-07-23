5f31d7d874dddfdd73473f7fa7be4ae70ae832af
stevenjobs530-afk <stevenjobs530@gmail.com>
2026-07-23 16:55:28 +0100

    Fix case study navigation on GitHub Pages

--- _next/static/chunks/app/layout-ad44a61688563be5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3974:()=>{},7337:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3974,23))}},e=>{e.O(0,[283,708,622,358],()=>e(e.s=7337)),_N_E=e.O()}]);
--- _next/static/chunks/next/dist/client/components/builtin/global-error-e2371eb734b72cda.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[337],{6505:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3179,23))}},e=>{e.O(0,[708,622,358],()=>e(e.s=6505)),_N_E=e.O()}]);
