7b2ef26af64470273f550d93e621fe1b94ab74af
stevenjobs530-afk <stevenjobs530@gmail.com>
2026-07-23 16:23:49 +0100

    Publish latest approved portfolio copy

---
+++ _next/static/chunks/app/layout-ad44a61688563be5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3974:()=>{},7337:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3974,23))}},e=>{e.O(0,[283,708,622,358],()=>e(e.s=7337)),_N_E=e.O()}]);
